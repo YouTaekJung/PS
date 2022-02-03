84451a027e7d4c8a0dcb3daf0187da78f5c968db
youtaekjung <jyt9319@gmail.com>
2022-02-03 12:17:18 +0900

    1929

--- Baekjoon/math/1929_소수_구하기.py
+++ Baekjoon/math/1929_소수_구하기.py
@@ -1,8 +1,12 @@
 m, n = map(int, input().split())
 
-for num in range(m, n + 1):
-    for i in range(2, num):
-        if num % i == 0:
-            break
-        if i == num - 1:
-            print(num)
+li = [0, 0] + [1] * (n - 1)
+
+for i in range(2, n+1):
+    if li[i]:
+        for j in range(2*i, n+1, i):
+            li[j] = 0
+
+for i in range(m, n+1):
+    if li[i]:
+        print(i)
