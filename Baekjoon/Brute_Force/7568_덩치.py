78ec45439e1753cd652517baea15b732baa0c9db
youtaekjung <jyt9319@gmail.com>
2022-02-09 17:37:28 +0900

    7568

--- Baekjoon/Brute_Force/7568_덩치.py
+++ Baekjoon/Brute_Force/7568_덩치.py
@@ -1,21 +1,13 @@
 n = int(input())
 
-li = [0]*n
-sorted_li = [[i, 0] for i in range(n)]
-
+li = [0] * n
 for i in range(n):
     li[i] = list(map(int, input().split()))
+res = [1] * n
 
 for i in range(n):
-    for j in range(i + 1, n):
+    for j in range(n):
         if li[i][0] > li[j][0] and li[i][1] > li[j][1]:
-            sorted_li[j][1] += 1
-        if li[i][0] < li[j][0] and li[i][1] < li[j][1]:
-            sorted_li[i][1] += 1
+            res[j] += 1
 
-sorted_li = sorted(sorted_li, key=lambda x: x[1])
-res = sorted_li[0][0] + [0] * (n - 1)
-cur = [sorted_li[0][1], 1]
-
-for i in range(1, n):
-    res[i]
+print(' '.join(map(str, res)))
