3b5c5359531b70e132a275cee9315622c5506771
youtaekjung <jyt9319@gmail.com>
2022-02-10 11:35:11 +0900

    2108

--- Baekjoon/sort/2108_통계학.py
+++ Baekjoon/sort/2108_통계학.py
@@ -1,33 +1,19 @@
-n = int(input())
+import sys
+from collections import Counter
+
+n = int(sys.stdin.readline())
 li = []
 
-for _ in range(n):
-    li.append(int(input()))
+for i in range(n):
+    li.append(int(sys.stdin.readline()))
 
-dic = {}
-for num in li:
-    if num in dic:
-        dic[num] += 1
-    else:
-        dic[num] = 0
-dic = sorted(dic.items(), key=lambda x: x[1], reverse=True)
+li.sort()
 
-end = 0
-max = dic[0][1]
-for i in range(1, len(dic)):
-    if max == dic[i][1]:
-        end += 1
-    else:
-        break
-
-li = sorted(li)
-
+li_s = Counter(li).most_common()
 print(round(sum(li) / n))
-print(li[int(n // 2)] if n % 2 else (li[int(n // 2)] + li[int(n // 2) - 1]) / 2)
-if n == 1:
-    print(li[0])
-elif dic[0][1] == 0:
-    print(li[1])
+print(li[n // 2])
+if len(li_s) > 1 and li_s[0][1] == li_s[1][1]:
+    print(li_s[1][0])
 else:
-    print(dic[end-1][0] if end else dic[0][0])
+    print(li_s[0][0])
 print(li[-1] - li[0])
