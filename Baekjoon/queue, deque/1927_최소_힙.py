6e4b879ead503281a5c14ab98be220ff9c1a54c9
youtaekjung <jyt9319@gmail.com>
2022-03-10 15:57:52 +0900

    1927

---
+++ Baekjoon/queue, deque/1927_최소_힙.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+h = []
+
+for _ in range(n):
+    cur = int(sys.stdin.readline())
+    if cur == 0:
+        if len(h) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(h))
+    else:
+        heapq.heappush(h, cur)
