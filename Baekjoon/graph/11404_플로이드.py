406949c4b6d14ebbee4f5a5449af948e13a30336
youtaekjung <jyt9319@gmail.com>
2022-03-17 17:23:18 +0900

    11404

--- Baekjoon/graph/11404_플로이드.py
+++ Baekjoon/graph/11404_플로이드.py
@@ -1,8 +1,9 @@
 import sys
 input = sys.stdin.readline
 
+INF = int(1e9)
 n, m = int(input()), int(input())
-graph = [[100001] * (n + 1) for _ in range(n + 1)]
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
 
 for _ in range(m):
     a, b, c = map(int, input().split())
@@ -15,4 +16,4 @@ for k in range(1, n + 1):
                 graph[i][j] = min(graph[i][k] + graph[k][j], graph[i][j])
 
 for g in graph[1:]:
-    print(' '.join(list(map(str, g[1:]))).replace('100001', '0'))
+    print(' '.join(list(map(str, g[1:]))).replace('1000000000', '0'))
