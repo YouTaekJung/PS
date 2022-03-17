7687d2234872d9bf34ca476b8dd662a266dfd3da
youtaekjung <jyt9319@gmail.com>
2022-03-17 17:05:51 +0900

    1753

--- Baekjoon/graph/1753_최단경로.py
+++ Baekjoon/graph/1753_최단경로.py
@@ -1,8 +1,26 @@
+import heapq
+
 v, e = map(int, input().split())
-graph = [[0] * (v + 1) for _ in range(v + 1)]
+s = int(input())
+graph = [[] for _ in range(v + 1)]
+distance = [200000] * (v + 1)
 
 for _ in range(e):
     x, y, k = map(int, input().split())
-    graph[x][y] = graph[y][x] = k
+    graph[x].append((y, k))
 
-def dijkstra
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, cur = heapq.heappop(q)
+        if distance[cur] >= dist:
+            for i in graph[cur]:
+                cost = dist + i[1]
+                if cost < distance[i[0]]:
+                    distance[i[0]] = cost
+                    heapq.heappush(q, (cost, i[0]))
+
+dijkstra(s)
+print('\n'.join(list(map(str, distance[1:]))).replace('200000', 'INF'))
