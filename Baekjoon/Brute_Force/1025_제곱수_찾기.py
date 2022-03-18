9dea899269f3bbaf8fffc6f9a8d08176f8a563dd
youtaekjung <jyt9319@gmail.com>
2022-03-18 13:24:49 +0900

    1025

--- Baekjoon/Brute_Force/1025_제곱수_찾기.py
+++ Baekjoon/Brute_Force/1025_제곱수_찾기.py
@@ -12,15 +12,17 @@ def bf(cur, x, y, dx, dy):
         cur += matrix[x][y]
         bf(cur, x, y, dx, dy)
 
-for i in range(n):
-    for j in range(m):
-        for dx in range(n):
-            for dy in range(m):
-                if dx != 0 or dy != 0:
-                    bf(matrix[i][j], i, j, dx, dy)
-                    bf(matrix[i][j], i, j, -dx, dy)
-                    bf(matrix[i][j], i, j, dx, -dy)
-                    bf(matrix[i][j], i, j, -dx, -dy)
-
-
-print(max(res) if len(res) else -1)
+if n == 1 and m == 1:
+    temp = int(matrix[0][0]) ** 0.5
+    print(matrix[0][0] if int(temp) == temp else -1)
+else:
+    for i in range(n):
+        for j in range(m):
+            for dx in range(n):
+                for dy in range(m):
+                    if dx != 0 or dy != 0:
+                        bf(matrix[i][j], i, j, dx, dy)
+                        bf(matrix[i][j], i, j, -dx, dy)
+                        bf(matrix[i][j], i, j, dx, -dy)
+                        bf(matrix[i][j], i, j, -dx, -dy)
+    print(max(res) if len(res) else -1)
