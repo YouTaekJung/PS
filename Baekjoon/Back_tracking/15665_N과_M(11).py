61b799a7af99dd4ecc2276454471c6c2a5a19263
youtaekjung <jyt9319@gmail.com>
2022-03-21 17:34:04 +0900

    15665

--- Baekjoon/Back_tracking/15665_N과_M(11).py
+++ Baekjoon/Back_tracking/15665_N과_M(11).py
@@ -1,21 +1,19 @@
 n, m = map(int, input().split())
 li = sorted(list(set(map(int, input().split()))))
 temp = []
-cache = []
+d = {}
 
 def dfs():
     if len(temp) == m:
-        cache.append(' '.join(list(map(str, temp))))
+        s = ' '.join(list(map(str, temp)))
+        if s not in d:
+            d[s] = 1
+            print(s)
         return
 
     for i in range(len(li)):
         temp.append(li[i])
-        c = li[i]
         dfs()
         temp.pop()
 
 dfs()
-
-cache = sorted(list(set(cache)))
-for c in cache:
-    print(c)
