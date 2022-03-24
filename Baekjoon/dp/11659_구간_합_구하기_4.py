cf3d6cb9b06babc10cf265982e7bc0c5bf789347
youtaekjung <jyt9319@gmail.com>
2022-03-25 00:08:14 +0900

    11659

--- Baekjoon/dp/11659_구간_합_구하기_4.py
+++ Baekjoon/dp/11659_구간_합_구하기_4.py
@@ -1,12 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().rstrip().split())
-li = list(map(int, input().rstrip().split()))
+n, m = map(int, input().split())
+li = list(map(int, input().split()))
 dp = [0] * (n + 1)
 for i in range(1, n + 1):
     dp[i] = dp[i - 1] + li[i - 1]
 
-for _ in range(n):
-    x, y = map(int, input().rstrip().split())
+for _ in range(m):
+    x, y = map(int, input().split())
     print(dp[y] - dp[x - 1])
