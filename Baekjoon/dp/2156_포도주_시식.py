fa1d28a2c9ecef6efcfa2255dfeb4ba94edba9f8
youtaekjung <jyt9319@gmail.com>
2022-03-30 14:04:15 +0900

    2156

--- Baekjoon/dp/2156_포도주_시식.py
+++ Baekjoon/dp/2156_포도주_시식.py
@@ -1,7 +1,10 @@
 n = int(input())
 li = [0] + [int(input()) for _ in range(n)]
-dp = [0, li[1], li[1] + li[2]]
+if n < 3:
+    print(sum(li))
+else:
+    dp = [0, li[1], li[1] + li[2]]
 
-for i in range(3, n + 1):
-    dp.append(max(dp[i - 1], dp[i - 3] + li[i - 1] + li[i], dp[i - 2] + li[i]))
-print(dp[-1])
+    for i in range(3, n + 1):
+        dp.append(max(dp[i - 1], dp[i - 3] + li[i - 1] + li[i], dp[i - 2] + li[i]))
+    print(dp[n])
