45b7cdc649b1f3dfd910edc63518ac5652d6bb9a
youtaekjung <jyt9319@gmail.com>
2022-06-19 14:28:02 +0900

    13560

--- Baekjoon/math/13560_축구_게임.py
+++ Baekjoon/math/13560_축구_게임.py
@@ -5,7 +5,7 @@ def check():
     s = 0
     if sum(li) != n * (n - 1) // 2:
         return -1
-    for i, l in enumerate(li):
+    for i, l in enumerate(li[:-1]):
         s += l
         if s < i * (i - 1) // 2:
             return -1
