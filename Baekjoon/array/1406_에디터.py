f3b49de7572ad512fd24015298d38e64dcd092be
youtaekjung <jyt9319@gmail.com>
2022-07-04 13:06:45 +0900

    1406

--- Baekjoon/array/1406_에디터.py
+++ Baekjoon/array/1406_에디터.py
@@ -1,23 +1,17 @@
 import sys
 input = sys.stdin.readline
-s = list(input().rstrip())
+s1, s2 = list(input().strip()), []
 n = int(input())
 
-
-cursor = len(s)
-for i in range(n):
+for _ in range(n):
     command = input().split()
-    if command[0] == 'L':
-        cursor = max(cursor - 1, 0)
-    elif command[0] == 'D':
-        cursor = min(len(s), cursor + 1)
-    elif command[0] == 'B':
-        if cursor != 0:
-            s.remove(s[cursor - 1])
-            cursor -= 1
-    else:
-        s.insert(cursor, command[1])
-        cursor += 1
-print(''.join(s))
+    if command[0] == 'P':
+        s1.append(command[1])
+    elif command[0] == 'L' and s1 != []:
+        s2.append(s1.pop())
+    elif command[0] == 'D' and s2 != []:
+        s1.append(s2.pop())
+    elif command[0] == 'B' and s1 != []:
+        s1.pop()
 
-
+print(''.join(s1 + s2[::-1]))
