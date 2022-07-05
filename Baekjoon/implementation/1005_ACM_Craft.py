14e5bb999f66d84faf364df2ebf5398a52b702ed
youtaekjung <jyt9319@gmail.com>
2022-07-05 09:27:08 +0900

    1005

--- Baekjoon/implementation/1005_ACM_Craft.py
+++ Baekjoon/implementation/1005_ACM_Craft.py
@@ -1,10 +1,37 @@
-t = int(input())
+from collections import deque
+import sys
+input = sys.stdin.readline
 
+def topological_sort():
+    dp = [0] * (n + 1)
+    q = deque()
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+            dp[i] = cost[i]
+
+    while q:
+        cur = q.popleft()
+
+        for g in graph[cur]:
+            indegree[g] -= 1
+            dp[g] = max(dp[g], dp[cur] + cost[g])
+            if indegree[g] == 0:
+                q.append(g)
+            if indegree[last] == 0:
+                print(dp[last])
+                return
+
+t = int(input())
 for _ in range(t):
     n, k = map(int, input().split())
-    time = list(map(int, input().split()))
-    li = [[] for _ in range(n)]
+    graph = [[] for _ in range(n + 1)]
+    cost = [0] +list(map(int, input().split()))
+    indegree = [0] * (n + 1)
+
     for _ in range(k):
-        s, n = map(int, input().split())
-        li[s - 1].append(n)
-    print(li)
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        indegree[b] += 1
+    last = int(input())
+    topological_sort()
