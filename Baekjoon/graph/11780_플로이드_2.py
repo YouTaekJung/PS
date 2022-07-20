842cd04fd23bdbaf169889571368cb43a2de73cb
youtaekjung <jyt9319@gmail.com>
2022-07-20 12:05:07 +0900

    11780

--- Baekjoon/graph/11780_플로이드_2.py
+++ Baekjoon/graph/11780_플로이드_2.py
@@ -3,17 +3,28 @@ input = sys.stdin.readline
 
 INF = int(1e9)
 n, m = int(input()), int(input())
-graph = [[INF] * (n + 1) for _ in range(n + 1)]
+graph = [[[INF, [i, j]] for j in range(n + 1)] for i in range(n + 1)]
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-    graph[a][b] = min(graph[a][b], c)
+    graph[a][b][0] = min(graph[a][b][0], c)
 
 for k in range(1, n + 1):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             if i != j:
-                graph[i][j] = min(graph[i][k] + graph[k][j], graph[i][j])
+                if graph[i][j][0] > graph[i][k][0] + graph[k][j][0]:
+                    graph[i][j][0] = graph[i][k][0] + graph[k][j][0]
+                    graph[i][j][1] = graph[i][k][1] + graph[k][j][1][1:]
+            else:
+                graph[i][j][0] = 0
 
 for g in graph[1:]:
-    print(' '.join(list(map(str, g[1:]))).replace('1000000000', '0'))
+    print(' '.join(list(map(lambda x: str(x[0]), g[1:]))))
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if graph[i][j][0] == 0:
+            print(0)
+        else:
+            print(len(graph[i][j][1]), *graph[i][j][1])
