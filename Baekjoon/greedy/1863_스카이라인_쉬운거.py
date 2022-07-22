2eaf9a78a5ca1124173b6bf80cbed9742b010fc6
youtaekjung <jyt9319@gmail.com>
2022-07-22 15:26:12 +0900

    1863

---
+++ Baekjoon/greedy/1863_스카이라인_쉬운거.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack, ans = [], 0
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    while stack and stack[-1] > y:
+        ans += 1
+        stack.pop()
+    if stack and stack[-1] == y:
+        continue
+    stack.append(y)
+
+while stack:
+    if stack[-1] > 0:
+        ans += 1
+    stack.pop()
+
+print(ans)
