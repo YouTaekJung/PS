cf31189e35594e00c875fba1fa3e674605abfcf8
youtaekjung <jyt9319@gmail.com>
2022-08-05 15:28:02 +0900

    24483

---
+++ Baekjoon/dfs, bfs/24483_알고리즘_수업-깊이_우선_탐색_5.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10 ** 8)
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [[-1, 0] for _ in range(n + 1)]
+count = 1
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+def dfs(node):
+    global count
+
+    graph[node].sort()
+    for g in graph[node]:
+        if visited[g][0] == -1:
+            count += 1
+            visited[g] = [visited[node][0] + 1, count]
+            dfs(g)
+
+visited[r][0] = 0
+dfs(r)
+
+ans = 0
+for v in visited[1:]:
+    ans += v[0] * v[1]
+print(ans)
