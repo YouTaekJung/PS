77144720769d8049a7828b2c1d9c83db267c6f7b
youtaekjung <jyt9319@gmail.com>
2022-08-26 11:29:24 +0900

    1976

--- Baekjoon/union_find/1976_여행_가자.py
+++ Baekjoon/union_find/1976_여행_가자.py
@@ -2,18 +2,12 @@ import sys
 input = sys.stdin.readline
 
 n, m = int(input()), int(input())
-s = list(range(n + 1))
+p = list(range(n + 1))
 
 def find_parent(parent, x):
     if parent[x] != x:
         return find_parent(parent, parent[x])
     return x
-
-def find_num(parent, x, y):
-    if x == y or find_parent(parent, x) == find_parent(parent, y):
-        print("YES")
-    else:
-        print("NO")
 
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
@@ -23,9 +17,15 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
-for i in range(m):
-    c, a, b = map(int, input().split())
-    if c == 0:
-        union_parent(s, a, b)
-    else:
-        find_num(s, a, b)
+for i in range(n):
+    li = list(map(int, input().split()))
+    for j in range(n):
+        if li[j]:
+            union_parent(p, i + 1, j + 1)
+
+road = list(map(int, input().split()))
+for i in range(m - 1):
+    if find_parent(p, road[i]) != find_parent(p, road[i + 1]):
+        print('NO')
+        exit()
+print('YES')
