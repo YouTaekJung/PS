42835adeace36a86842f46f6461ed87833a8164f
youtaekjung <jyt9319@gmail.com>
2022-09-15 20:54:54 +0900

    1253

--- Baekjoon/search/1253_좋다.py
+++ Baekjoon/search/1253_좋다.py
@@ -1,15 +1,16 @@
 n = int(input())
 li = sorted(list(map(int, input().split())))
-check = [0] * n
 
+ans = 0
 for cur in range(n):
-    left, right = 0, n - 1
+    temp = li[:cur] + li[cur + 1:]
+    left, right = 0, len(temp) - 1
     while left < right:
-        if li[left] + li[right] == li[cur]:
-            check[cur] = 1
+        if temp[left] + temp[right] == li[cur]:
+            ans += 1
+            break
+        if temp[left] + temp[right] < li[cur]:
             left += 1
-        elif li[left] + li[right] > li[cur]:
-            right -= 1
         else:
-            left += 1
-print(check.count(1))
+            right -= 1
+print(ans)
