c34e17599586271b7c36be6eff7bbedb0817f024
youtaekjung <jyt9319@gmail.com>
2022-09-22 03:38:31 +0900

    9657

---
+++ Baekjoon/implementation/9657_돌_게임_3.py
@@ -0,0 +1,9 @@
+n = int(input())
+dp = [0] * (n + 1)
+dp[1] = dp[3] = dp[4] = 1
+
+for i in range(5, n + 1):
+    if not min(dp[i - 1], dp[i - 3], dp[i - 4]):
+        dp[i] = 1
+
+print('SK' if dp[n] else 'CY')
