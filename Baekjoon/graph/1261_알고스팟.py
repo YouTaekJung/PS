932500e59d0aa364d11828915b0db2f7b06643d3
youtaekjung <jyt9319@gmail.com>
2022-10-01 19:27:32 +0900

    1261

--- Baekjoon/graph/1261_알고스팟.py
+++ Baekjoon/graph/1261_알고스팟.py
@@ -3,18 +3,19 @@ from collections import deque
 n, m = map(int, input().split())
 board = [list(map(int, list(input()))) for _ in range(m)]
 visited = [[0] * n for _ in range(m)]
+visited[0][0] = 1
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 
 q = deque([[0, 0, 0]])
 while q:
     x, y, c = q.popleft()
-    visited[x][y] = 1
     if x == m - 1 and y == n - 1:
         print(c)
         break
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
         if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny]:
+            visited[nx][ny] = 1
             if board[nx][ny] == 1:
                 q.append([nx, ny, c + 1])
             else:
