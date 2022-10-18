4707b69a5a9c71dd5259362e805698a691ea5f22
youtaekjung <jyt9319@gmail.com>
2022-10-18 18:02:08 +0900

    1749

--- Baekjoon/dp/1749_점수따먹기.py
+++ Baekjoon/dp/1749_점수따먹기.py
@@ -1,24 +1,19 @@
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
-
-def sum_board(board):
-    s = [[sum(board[i][:j + 1]) for j in range(len(board[0]))]
-                for i in range(len(board))]
-
-    for i in range(len(s) - 1):
-        for j in range(len(s[0])):
-            s[i + 1][j] += s[i][j]
-
-    return s
+prefix_sum = [[0] * (m + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        prefix_sum[i][j] = prefix_sum[i - 1][j] + prefix_sum[i][j - 1] + \
+                           board[i - 1][j - 1] - prefix_sum[i - 1][j - 1]
 
 def acc(mat, x1, y1, x2, y2):
-    return mat[x2][y2] - mat[x1][y2] - mat[x2][y1] + mat[x1][y1]
+    return mat[x2][y2] - mat[x1 - 1][y2] - mat[x2][y1 - 1] + mat[x1 - 1][y1 - 1]
 
-mat, ans = sum_board(board), -1000000000
-for x1 in range(n):
-    for y1 in range(m):
-        for x2 in range(x1 + 1, n):
-            for y2 in range(y1 + 1, m):
-                ans = max(ans, acc(mat, x1, y1, x2, y2))
+ans = -40000
+for x1 in range(1, n + 1):
+    for y1 in range(1, m + 1):
+        for x2 in range(x1, n + 1):
+            for y2 in range(y1, m + 1):
+                ans = max(ans, acc(prefix_sum, x1, y1, x2, y2))
 
 print(ans)
