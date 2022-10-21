42971604e46d865a3c0d5fa72fee4fc5e1bce064
youtaekjung <jyt9319@gmail.com>
2022-10-21 09:00:00 +0900

    1105

---
+++ Baekjoon/string/1105_팔.py
@@ -0,0 +1,14 @@
+l, r = input().split()
+
+if len(l) != len(r):
+    print(0)
+else:
+    ans = 0
+    for i in range(len(l)):
+        if l[i] == r[i]:
+            if l[i] == '8':
+                ans += 1
+        else:
+            break
+
+    print(ans)
