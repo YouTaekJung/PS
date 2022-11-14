de6add474e7b62b8e4b794e7809896fe28f72d3e
youtaekjung <jyt9319@gmail.com>
2022-11-14 13:29:54 +0900

    1298

---
+++ Baekjoon/dfs, bfs/1298_노트북의_주인을_찾아서.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+def bimatch(num):
+    if visited[num]:
+        return False
+    visited[num] = 1
+    for g in graph[num]:
+        if selected[g] == -1 or bimatch(selected[g]):
+            selected[g] = num
+            return True
+    return False
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n)]
+selected = [-1] * (m + 1)
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x - 1].append(y - 1)
+
+ans = 0
+for i in range(n):
+    visited = [0] * n
+    if bimatch(i):
+        ans += 1
+print(ans)
