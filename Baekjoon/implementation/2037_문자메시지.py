7be4ad6f1325ddc34273c4ffe0b804646a253f5a
youtaek <youtaek@youtaekui-MacBookPro.local>
2023-01-22 14:29:27 +0900

    2037

---
+++ Baekjoon/implementation/2037_문자메시지.py
@@ -0,0 +1,18 @@
+key = ['', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+p, w = map(int, input().split())
+text = input()
+
+ans = 0
+prev = ''
+for i in range(len(text)):
+    if text[i] == ' ':
+        ans += p
+        prev = ''
+    for j in range(len(key)):
+        for k in range(len(key[j])):
+            if text[i] == key[j][k]:
+                if j == prev:
+                    ans += w
+                ans += (1 + k) * p
+                prev = j
+print(ans)
