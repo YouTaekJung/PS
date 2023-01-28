73bc9536b67d342a8dfbec541986ab24841025e1
youtaekjung <jyt9319@gmail.com>
2023-01-28 13:18:36 +0900

    27160

---
+++ Baekjoon/implementation/27160_할리갈리.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+d = {}
+for _ in range(int(input())):
+    s, x = input().split()
+    d[s] = d.get(s, 0) + int(x)
+
+for n in d:
+    if d[n] == 5:
+        print('YES')
+        exit()
+print('NO')
