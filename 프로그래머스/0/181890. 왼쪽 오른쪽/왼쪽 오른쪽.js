981495d057fdb13f548cc2a3a106be71f87f7639
takeU <jyt9319@gmail.com>
2025-02-14 13:12:22 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -0,0 +1,7 @@
+function solution(str_list) {
+    const idx = str_list.findIndex(item => item === 'l' || item === 'r')
+    
+    if (idx === -1 ) return []
+    else if ( str_list[idx] === 'l' ) return str_list.slice(0, idx)    
+    return str_list.slice(idx + 1)
+}
