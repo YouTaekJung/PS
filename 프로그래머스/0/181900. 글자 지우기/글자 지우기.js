8796d4a79f41828dcf74fdae1bd610e21bb50957
takeU <jyt9319@gmail.com>
2025-02-15 14:49:52 +0900

    [level 0] Title: 글자 지우기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -1,9 +1,4 @@
 function solution(my_string, indices) {
-    let res = ''
-    indices.sort((a, b) => a - b)
-    for (let i = 0; i < my_string.length; i++) {
-        if (indices[0] === i) indices.shift()
-        else res += my_string[i]
-    }
-    return res
+    var answer = '';
+    return answer;
 }
