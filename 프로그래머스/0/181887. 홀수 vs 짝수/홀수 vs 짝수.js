67efc75b7414eaf8def5734ab21e3728a9b3ef13
takeU <jyt9319@gmail.com>
2025-02-18 22:39:47 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,6 @@
+function solution(num_list) {
+    return Math.max(
+        num_list.reduce((acc, cur, idx) => idx % 2 ? acc + cur : acc, 0),
+        num_list.reduce((acc, cur, idx) => idx % 2 ? acc : acc + cur, 0)
+    )
+}
